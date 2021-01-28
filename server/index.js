63dc49ca7861f8761642cea2e076dc797f4a3d8a
Dan Ngetich <dancanmilgo73@gmail.com>
2021-01-28 16:52:26 +0300

    updates

--- server/index.js
+++ server/index.js
@@ -1,22 +1,42 @@
+//const REDIS_URL = "redis://:pdb365798c135c083b093000aaf1105282694eb5a2f7c210fbbb3d3d667c56418@ec2-54-75-41-243.eu-west-1.compute.amazonaws.com:9529"
 const express = require('express')
+require('dotenv').config()
 const app = express()
-const port = 3001
-const redis = require("redis");
-const client = redis.createClient({
-  host: '127.0.0.1',
-  port: 6379
-});
+const port = process.env.PORT || 3001
 
-const { promisify } = require("util");
-const getAsync = promisify(client.get).bind(client);
+const { MongoClient } = require("mongodb");
+ 
+// Replace the following with your Atlas connection string                                                                                                                                        
+const url = "mongodb+srv://dancan:Dancan2020@search--jobs.sp1it.mongodb.net/search--jobs?retryWrites=true&w=majority";
+const client = new MongoClient(url,{ useUnifiedTopology: true });
 
-app.get('/jobs', async (req, res) => {
-  const jobs = await getAsync("jobsToStore"); 
-  res.header("Access-Control-Allow-Origin", "  http://localhost:3000");
-  console.log(JSON.parse(jobs).length); 
-  res.send(jobs.replace(/(<([^>]+)>)/gi, ""))
-})
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("search--jobs");
+    const collection = database.collection("jobsToStore");
+  const cursor = collection.find({});  
+    // print a message if no documents were found
+    if ((await cursor.count()) === 0) {
+      console.log("No documents found!");
+    }
+    // get jobs
+    app.get('/jobs', async (req, res) => {
+       const jobs = await cursor.toArray();
+       res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+       //console.log(JSON.parse(jobs).length); 
+       res.send(jobs);//.replace(/(<([^>]+)>)/gi, ""))
+     })
+    
+  } finally {
+    //await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`);
 })
--- server/worker/fetchTask/fetchJobs.js
+++ server/worker/fetchTask/fetchJobs.js
@@ -1,11 +1,21 @@
 const fetch = require('node-fetch');
 const redis = require("redis");
-const client = redis.createClient({
+require('dotenv').config();
+const credentials = {
+  port: "13869",
+  url: "redis-13869.c135.eu-central-1-1.ec2.cloud.redislabs.com:13869",
+  password: "T6454i7er6v09nk2IqZ4LakMPkul7dvF",
+  syscall: "0.0.0.0"
+}
+/* const client = redis.createClient({
   host: '127.0.0.1',
   port: 6379
-});
+}); */
+//let REDIS_URL = process.env.REDIS_URL;
+//const client = redis.createClient(process.env.REDIS_URL);
 
-const { promisify } = require("util");
+
+/* const { promisify } = require("util");
 const setAsync = promisify(client.set).bind(client);
 
 async function fetchGit(){
@@ -30,11 +40,53 @@ async function fetchGit(){
    } catch(e) {
     console.log('Error happened while fetching the API: ', e.message)
    }
-   
+   client.on('connect', function(err, res) {
+    console.log('redis is connected!');
+  });
    client.on("error", function(error) {
-    console.error(error);
+    console.error(error.message);
   });
   
-  }
+  } */
+  
 //fetchGit();
+//module.exports = fetchGit;
+const { MongoClient } = require("mongodb");
+ 
+// Replace the following with your Atlas connection string                                                                                                                                        
+const url = "mongodb+srv://dancan:Dancan2020@search--jobs.sp1it.mongodb.net/search--jobs?retryWrites=true&w=majority";
+const client = new MongoClient(url,{ useUnifiedTopology: true });
+async function fetchGit() {
+    try {
+        await client.connect();
+        console.log("Connected correctly to server");
+        const db = client.db("search--jobs");
+        const col = db.collection("jobsToStore");
+        const options = { ordered: true };
+        const APIUrl = "https://jobs.github.com/positions.json";
+        let page = 0;
+        let numOfJobs = 0;
+        const jobs = [];
+          do{
+            const res = await fetch(`${APIUrl}?page=${page}`);
+            var data = await res.json();
+            numOfJobs = data.length;
+            jobs.push(...data);
+            page++;
+          }while(numOfJobs > 0);
+      
+      console.log(jobs.length);
+      const result = await col.insertMany(jobs, options);
+      console.log(`${result.insertedCount} documents were inserted`);
+      //const myDoc = await col.findOne();
+      //console.log(myDoc);
+        
+    } catch (err) {
+        console.log(err.stack);
+    }
+    finally {
+        await client.close();
+    }
+}
+fetchGit().catch(console.dir);
 module.exports = fetchGit;
